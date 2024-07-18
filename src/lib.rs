2bd06c2db8b5497dbee8eb6593383e11ba42e6c5
falcon71181 <abhaythakur71181@gmail.com>
2024-07-18 11:20:09 +0530

    feat: gigasay without text border layout

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+
 pub struct Options {
     pub width: u32,
 }
@@ -6,14 +8,10 @@ struct Chars {
     arrow: &'static str,
     top: &'static str,
     bottom: &'static str,
-    left: &'static str,
-    right: &'static str,
+    line: &'static str,
     single_left: &'static str,
     single_right: &'static str,
-    angled_up_right: &'static str,
-    angled_up_left: &'static str,
-    angled_down_right: &'static str,
-    angled_down_left: &'static str,
+    angled_up: &'static str,
 }
 
 static GIGACHAD: &str = "
@@ -40,15 +38,29 @@ static SAY_CHARS: Chars = Chars {
     arrow: "\\",
     top: "-",
     bottom: "- ",
-    left: "|",
-    right: "|",
+    line: "|",
     single_left: "<",
     single_right: ">",
-    angled_up_right: "/",
-    angled_up_left: "\\",
-    angled_down_right: "\\",
-    angled_down_left: "/",
+    angled_up: "/",
 };
+
+fn generate_gigasay_message(message: &str, opts: Options) -> String {
+    format!(
+        "
+    {}
+  {}
+ {}
+  {}
+   {}
+    {}",
+        message,
+        SAY_CHARS.angled_up,
+        SAY_CHARS.single_left,
+        SAY_CHARS.arrow,
+        SAY_CHARS.arrow,
+        SAY_CHARS.arrow
+    )
+}
 
 fn closest_no_multiple_of_four(n: &mut usize) -> usize {
     if *n == 0 {
@@ -61,7 +73,7 @@ fn closest_no_multiple_of_four(n: &mut usize) -> usize {
 }
 
 pub fn print_message(message: &str, opts: Options) {
-    println!("{}{}", message, GIGACHAD);
+    println!("{}{}", generate_gigasay_message(message, opts), GIGACHAD);
 }
 
 #[cfg(test)]
